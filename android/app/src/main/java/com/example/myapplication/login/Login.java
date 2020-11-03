fac93421f375fe5149c31d132edab20a15e3f795
Viorel Iordache <iordacheviorel99@gmail.com>
2020-11-03 22:33:10 +0200

    abstract login

---
+++ android/app/src/main/java/com/example/myapplication/login/Login.java
@@ -0,0 +1,12 @@
+public class Login
+{
+    public Result<User> login(String username, String password) {
+
+        // TODO
+    }
+
+    public void logout(String id) {
+
+        // TODO
+    }
+}
